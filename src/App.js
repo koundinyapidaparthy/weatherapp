a61abe46bef5311f8b1b0a6d0cdc66d60a3d91b0
koundinyapidaparthy <63140366+koundinyapidaparthy@users.noreply.github.com>
2021-06-13 01:44:12 +0530

    wrong push details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {BrowserRouter as Router,Route,Switch,} from  "react-router-dom";
-import Default from "./components/home/HomeWeather";
+import Daa from "./components/home/HomeWeather";
 function App() {
   return (
     <Router>
     <Switch>
-      <Route exact path="/">
-      <Default />
+      <Route exact path="/weatherapp">
+      <Daa />
       </Route>
     </Switch>
   </Router>
